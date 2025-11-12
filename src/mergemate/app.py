20f0c1c63a3508d366777185894e15cebd7c54eb
Oleg Patsay <smmartbiz@gmail.com>
2025-11-12 05:00:19 +0300

    refactor: remove MergeMateApp and associated main functionality

--- src/mergemate/app.py
+++
@@ -1,6 +0,0 @@
-class MergeMateApp:
-    def __init__(self):
-        print("MergeMateApp initialized")
-
-    def run(self):
-        print("MergeMateApp is running")
--- src/mergemate/main.py
+++
@@ -1,9 +0,0 @@
-from mergemate.app import MergeMateApp
-
-
-def main():
-    main = MergeMateApp()
-    main.run()
-
-if __name__ == "__main__":
-    main()
